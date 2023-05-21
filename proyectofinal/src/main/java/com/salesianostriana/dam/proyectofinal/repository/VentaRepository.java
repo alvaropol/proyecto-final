f9a7b77d133ae0e29ba9d5b1e6013df5b35c9bb0
Álvaro Polonio Domínguez <alvaro.pold@gmail.com>
2023-05-22 00:58:40 +0200

    funcionalidades consultas

--- proyectofinal/src/main/java/com/salesianostriana/dam/proyectofinal/repository/VentaRepository.java
+++ proyectofinal/src/main/java/com/salesianostriana/dam/proyectofinal/repository/VentaRepository.java
@@ -1,10 +1,14 @@
 package com.salesianostriana.dam.proyectofinal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.salesianostriana.dam.proyectofinal.model.Venta;
 
 public interface VentaRepository
 				extends JpaRepository<Venta, Long>{
+	
+   @Query("select sum(v.precioTotal) from Venta v WHERE v.socio.id = ?1")
+   public double getTotalGastadoPorIdSocio(Long idSocio);
 
 }
